cc2dabb1a22f0dd0c88caad903f59bd6c311b8bb
DonSera <sera.2021@lifeoasis.com>
2021-10-07 12:13:41 +0900

    feat : 지정한 id를 서버(3001)에 넘기면 결과 메세지를 받는다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <div>blank app</div>
-    </div>
-  );
+    const server = 'http://localhost:3001';
+    // const server = ''
+
+    const sendRequest = async () => {
+        const id = 7;
+        try {
+            axios.get(`${server}/button?id=${id}`).then(res => {
+                console.log('res : ' + res.data.message);
+            })
+        } catch (err) {
+            if (err) console.log(err)
+        }
+    }
+
+    return (
+        <div className="App">
+            <button onClick={() => sendRequest()}>Click</button>
+        </div>
+    );
 }
 
 export default App;
