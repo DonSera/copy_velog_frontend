fe8bbb86e4d38de5fdb6715c0cfa64bfd00c2651
DonSera <sera.2021@lifeoasis.com>
2021-10-13 16:19:13 +0900

    feat : 마이페이지 버튼 만들기 - 로그아웃 마이페이지 버튼 안으로 넣기

--- src/structure/header/Header.js
+++ src/structure/header/Header.js
@@ -1,21 +1,68 @@
 import styles from './Header.module.css'
 import {useState} from "react";
 import {useSelector} from "react-redux";
+import LoginButton from "../../components/buttons/LoginButton";
 
 function Header({loginInfo, handleHistory}) {
     const userInfo = useSelector(state => state.userInfo);
     const [openMenu, setOpenMenu] = useState(false);
+    const [openMyPageMenu, setOpenMyPageMenu] = useState(false);
 
-    function renderLoginButton() {
-        if (userInfo.email) {
-            return loginInfo.button.logged;
+    function handleMenu(type = 'menu') {
+        if (type === 'my page') {
+            setOpenMyPageMenu(!openMyPageMenu);
         } else {
-            return loginInfo.button.notLogged;
+            setOpenMenu(!openMenu);
         }
     }
 
-    function handleMenu() {
-        setOpenMenu(!openMenu);
+    function logoutClick() {
+        loginInfo.button.logged();
+        handleMenu('my page');
+    }
+
+    function renderLoginButton() {
+        if (userInfo.email) {
+            return <>
+                {userInfo.email}
+                {userInfo.name}
+                <button className={styles['example-button']}
+                        type={"button"}
+                        onClick={() => handleMenu('my page')}>
+                    마이페이지
+                </button>
+            </>
+        } else {
+            return <>
+                <LoginButton text={'Log in'} clickLogin={loginInfo.button.notLogged}/>
+                <LoginButton text={'Sign up'} clickLogin={loginInfo.button.notLogged}/>
+            </>
+        }
+    }
+
+    function menu() {
+        return <span className={styles['menu-board']}>
+                    <button type={"button"}
+                            onClick={() => handleHistory('notice')}>
+                        공지사항
+                    </button>
+                    <button type={"button"}
+                            onClick={() => handleHistory('tag_list')}>
+                        태그 목록
+                    </button>
+                    <button>서비스 정책</button>
+                    <button>Slack</button>
+                </span>
+    }
+
+    function myPageMenu() {
+        return <span className={styles['menu-board']}>
+                    <button>내 벨로그</button>
+                    <button>임시 글</button>
+                    <button>읽기 목록</button>
+                    <button>설정</button>
+                    <button onClick={logoutClick}>로그아웃</button>
+                </span>
     }
 
     return <>
@@ -29,9 +76,11 @@ function Header({loginInfo, handleHistory}) {
                     </button>
                 </span>
                 <span className={styles['login-box']}>
-                    {userInfo.email}
                     {renderLoginButton()}
                 </span>
+            </section>
+            <section className={styles['my-page-menu']}>
+                {openMyPageMenu && myPageMenu()}
             </section>
             <section className={styles['header-bottom']}>
                 <span className={styles['section']}>
@@ -54,21 +103,8 @@ function Header({loginInfo, handleHistory}) {
                 </span>
             </section>
             <section className={styles['header-menu']}>
-                {openMenu && <span className={styles['menu-board']}>
-                        <button type={"button"}
-                                onClick={() => handleHistory('notice')}>
-                            공지사항
-                        </button>
-                        <button type={"button"}
-                                onClick={() => handleHistory('tag_list')}>
-                            태그 목록
-                        </button>
-                        <button>서비스 정책</button>
-                        <button>Slack</button>
-                    </span>
-                }
+                {openMenu && menu()}
             </section>
-
         </div>
     </>
 }
