db601da07c76adff877dfe96568f946cdca5c4d5
DonSera <sera.2021@lifeoasis.com>
2021-10-15 14:56:34 +0900

    fix : key 없는 warning 해결

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,19 +3,21 @@ import styles from './Menu.module.css'
 function Menu({config}) {
     // config = [ [버튼이름, 클릭시 함수], ...]
     return <div className={styles['menu']}>
-                {config.map(config => {
-                    if (config.length > 1) {
-                        return <button type={"button"}
-                                       onClick={config[1]}>
-                            {config[0]}
-                        </button>
-                    } else {
-                        return <button type={"button"}>
-                            {config[0]}
-                        </button>
-                    }
-                })}
-                </div>
+        {config.map((config, index) => {
+            if (config.length > 1) {
+                return <button key={`menu_button_${index}`}
+                               type={"button"}
+                               onClick={config[1]}>
+                    {config[0]}
+                </button>
+            } else {
+                return <button key={`menu_button_${index}`}
+                               type={"button"}>
+                    {config[0]}
+                </button>
+            }
+        })}
+    </div>
 }
 
 export default Menu;
