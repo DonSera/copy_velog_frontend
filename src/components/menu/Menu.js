35dd7459a89140525f8e90efb6dafcccfa883894
DonSera <sera.2021@lifeoasis.com>
2021-10-15 15:29:11 +0900

    refactor : menu의 바깥 부분을 누르면 창이 꺼진다. - esc를 눌러도 꺼진다. - wraning 발생

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,6 +1,33 @@
 import styles from './Menu.module.css'
+import {useEffect, useRef} from "react";
 
-function Menu({config}) {
+function Menu({config, closeMenu}) {
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        document.onkeydown = function (event) {
+            if (event.key === 'Escape') {
+                closeMenu();
+            }
+        }
+
+        window.addEventListener('click', handleRef);
+        return () => {
+            window.removeEventListener('click', handleRef);
+        }
+    })
+
+    function close() {
+        closeMenu();
+        menuRef.current = null;
+    }
+
+    function handleRef(e) {
+        if (!menuRef.current?.contains(e.target)) {
+            close();
+        }
+    }
+
     // config = [ [버튼이름, 클릭시 함수], ...]
     return <div className={styles['menu']}>
         {config.map((config, index) => {
--- src/structure/header/Header.js
+++ src/structure/header/Header.js
@@ -1,5 +1,5 @@
 import styles from './Header.module.css'
-import {useEffect, useRef, useState} from "react";
+import {useState} from "react";
 import {useSelector} from "react-redux";
 import LoginButton from "../../components/buttons/LoginButton";
 import Menu from "../../components/menu/Menu";
@@ -9,48 +9,27 @@ function Header({loginInfo, handleHistory}) {
     const [openMenu, setOpenMenu] = useState(false);
     const [openMyPageMenu, setOpenMyPageMenu] = useState(false);
 
-    const menuRef = useRef(null);
-
-    useEffect(() => {
-        if (openMyPageMenu || openMenu) {
-            // 해당 파일이 열려 있을 때만 listener를 실행한다.
-            window.addEventListener('click', handleRef);
-            return () => {
-                window.removeEventListener('click', handleRef);
-            }
-        }
-    })
-
-    function handleRef(e) {
-        // 현재 내가 지정한 위치가(menuRef) 내가 클릭(click)한 곳에 포함이 되는가
-        // Ref의 current 값을 존재하지 않을 수 있으므로 ?를 사용한다.
-        if ((openMyPageMenu || openMenu) && !menuRef.current?.contains(e.target)) {
-            openMenu && handleMenu();
-            openMyPageMenu && handleMenu('my page');
-        }
+    function handleMenu() {
+        setOpenMenu(!openMenu);
     }
 
-    function handleMenu(type = 'menu') {
-        if (type === 'my page') {
-            setOpenMyPageMenu(!openMyPageMenu);
-        } else {
-            setOpenMenu(!openMenu);
-        }
-        // ref 값이 존재하는 경우 비운다.
-        if (!menuRef.current) menuRef.current = null;
+    function handleMyPageMenu() {
+        setOpenMyPageMenu(!openMyPageMenu);
     }
 
     function logoutClick() {
-        loginInfo.buttonFunction.logged();
-        handleMenu('my page');
+        loginInfo.buttonFunction.logout();
+        handleMyPageMenu();
     }
 
     function menu() {
         function gotoNotice() {
+            handleMenu();
             return handleHistory('notice');
         }
 
         function gotoTag() {
+            handleMenu();
             return handleHistory('tag_list')
         }
 
@@ -60,13 +39,12 @@ function Header({loginInfo, handleHistory}) {
             ['서비스 정책'],
             ['Slack']
         ];
-        return <div ref={menuRef}>
-            <Menu config={menuConfig}/>
-        </div>;
+        return <Menu config={menuConfig} closeMenu={handleMenu}/>;
     }
 
     function myPageMenu() {
         function gotoSetting() {
+            handleMyPageMenu();
             return handleHistory(`my_setting`);
         }
 
@@ -77,9 +55,7 @@ function Header({loginInfo, handleHistory}) {
             ['설정', gotoSetting],
             ['로그아웃', logoutClick]
         ];
-        return <span ref={menuRef}>
-            <Menu config={menuConfig}/>
-        </span>;
+        return <Menu config={menuConfig} closeMenu={handleMyPageMenu}/>;
     }
 
     return <div id={'header'}>
@@ -96,14 +72,14 @@ function Header({loginInfo, handleHistory}) {
                     ?
                     <div>
                         <button type={"button"}
-                                onClick={() => handleMenu('my page')}>
+                                onClick={handleMyPageMenu}>
                             마이페이지
                         </button>
                         {openMyPageMenu && myPageMenu()}
                     </div>
                     : <section>
-                        <LoginButton text={'Log in'} clickLogin={loginInfo.buttonFunction.notLogged}/>
-                        <LoginButton text={'Sign up'} clickLogin={loginInfo.buttonFunction.notLogged}/>
+                        <LoginButton text={'Log in'} clickLogin={loginInfo.buttonFunction.openModal}/>
+                        <LoginButton text={'Sign up'} clickLogin={loginInfo.buttonFunction.openModal}/>
                     </section>
                 }
             </section>
