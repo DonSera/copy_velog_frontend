a4dd48001be49607d091b5141d3137ddd087d30e
DonSera <sera.2021@lifeoasis.com>
2021-10-15 21:10:55 +0900

    feat : header의 움직임 조작 만들기

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import styles from './Header.module.css'
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import LoginButton from "../buttons/LoginButton";
 import Menu from "../menu/Menu";
@@ -13,6 +13,48 @@ function Header() {
     const userInfo = useSelector(state => state.userInfo);
     const [openMenu, setOpenMenu] = useState(false);
     const [openMyPageMenu, setOpenMyPageMenu] = useState(false);
+
+    const headerRef = useRef();
+    const preDir = useRef("down");
+    const preScrollY = useRef(0);
+    const lastScrollTop = useRef(0);
+
+    useEffect(() => {
+        window.addEventListener('scroll', updateScroll);
+        return () => {
+            window.removeEventListener('scroll', updateScroll);
+        }
+    })
+
+    function updateScroll() {
+        const currentScrollY = window.scrollY;
+        const height = Number(headerRef.current.style.height.split("px")[0]);
+        const marginTop = Number(headerRef.current.style.marginTop.split("px")[0]);
+
+        if (currentScrollY > lastScrollTop.current) {
+            console.log(marginTop + " down");
+            if (marginTop > -height) {
+                headerRef.current.style.marginTop = `-${currentScrollY - preScrollY.current}px`;
+            }
+            if (preDir.current === "up") {
+                console.log("change dir")
+                preScrollY.current = currentScrollY;
+            }
+            preDir.current = "down";
+        } else {
+            console.log(marginTop + " up");
+            if (marginTop < 0) {
+                headerRef.current.style.marginTop = `-${height - (preScrollY.current - currentScrollY)}px`;
+            }
+            if (preDir.current === "down") {
+                console.log("change dir")
+                preScrollY.current = currentScrollY;
+            }
+            preDir.current = "up";
+        }
+
+        lastScrollTop.current = currentScrollY;
+    }
 
     function handleHistory(loc) {
         history.push(`/${loc}`);
@@ -71,48 +113,52 @@ function Header() {
         return <Menu config={menuConfig} closeMenu={handleMyPageMenu}/>;
     }
 
-    return <div id={'header'}>
-        <section className={styles['header-top']}>
-            <section className={styles['left']}>
-                <button className={styles['logo']}
-                        onClick={() => handleHistory('')}>
-                    velog
-                </button>
-            </section>
-            <section className={styles['right']}>
-                {userInfo.name}
-                {userInfo.email
-                    ?
-                    <div>
-                        <button type={"button"}
-                                onClick={handleMyPageMenu}>
-                            마이페이지
-                        </button>
-                        {openMyPageMenu && myPageMenu()}
-                    </div>
-                    : <section>
-                        <LoginButton text={'Log in'} clickLogin={openClick}/>
-                        <LoginButton text={'Sign up'} clickLogin={openClick}/>
-                    </section>
-                }
-            </section>
-        </section>
-        <section className={styles['header-bottom']}>
-            <section className={styles['left']}>
-                <button type={"button"}>트렌딩</button>
-                <button type={"button"}>최신</button>
-                <button type={"button"}>일자</button>
-            </section>
-            <section className={styles['right']}>
-                <div>
-                    <button onClick={handleMenu}>
-                        메뉴
+    function renderHeader() {
+        return <div id={'header'} ref={headerRef} style={{height: "110px"}}>
+            <section className={styles['header-top']}>
+                <section className={styles['left']}>
+                    <button className={styles['logo']}
+                            onClick={() => handleHistory('')}>
+                        velog
                     </button>
-                    {openMenu && menu()}
-                </div>
+                </section>
+                <section className={styles['right']}>
+                    {userInfo.name}
+                    {userInfo.email
+                        ?
+                        <div>
+                            <button type={"button"}
+                                    onClick={handleMyPageMenu}>
+                                마이페이지
+                            </button>
+                            {openMyPageMenu && myPageMenu()}
+                        </div>
+                        : <section>
+                            <LoginButton text={'Log in'} clickLogin={openClick}/>
+                            <LoginButton text={'Sign up'} clickLogin={openClick}/>
+                        </section>
+                    }
+                </section>
             </section>
-        </section>
-    </div>
+            <section className={styles['header-bottom']}>
+                <section className={styles['left']}>
+                    <button type={"button"}>트렌딩</button>
+                    <button type={"button"}>최신</button>
+                    <button type={"button"}>일자</button>
+                </section>
+                <section className={styles['right']}>
+                    <div>
+                        <button onClick={handleMenu}>
+                            메뉴
+                        </button>
+                        {openMenu && menu()}
+                    </div>
+                </section>
+            </section>
+        </div>
+    }
+
+    return renderHeader();
 }
 
 export default Header;
