d4103e2d45d86f6465868cde334dc9bac4945bcd
DonSera <sera.2021@lifeoasis.com>
2021-10-18 14:34:56 +0900

    fix : 사용자 버튼 더블 클릭 불가로 만들기

--- src/components/buttons/LoginButton.js
+++ src/components/buttons/LoginButton.js
@@ -1,9 +1,20 @@
 import styles from './LoginButton.module.css'
+import {useState} from "react";
 
 function LoginButton({clickLogin, text}) {
+    const [doubleClickFlag, setDCFlag] = useState(false);
+
+    function onClick() {
+        if (!doubleClickFlag) {
+            setDCFlag(true);
+            clickLogin();
+            setDCFlag(false);
+        }
+    }
+
     return <button type={"button"}
                    className={styles[`login-button`]}
-                   onClick={clickLogin}>
+                   onClick={onClick}>
         {text}
     </button>
 }
