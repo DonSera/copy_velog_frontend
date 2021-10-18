786ca5efd8d1726ec2398fd202df465e55d64a4b
DonSera <sera.2021@lifeoasis.com>
2021-10-18 14:05:06 +0900

    feat : props 정보를 기반으로 정보 넣기

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,18 +1,20 @@
 import styles from './Board.module.css'
 
-function Board() {
+function Board({info}) {
     return <div className={styles['board']}>
         <div className={styles['image-wrap']}>
-            <div className={styles['image']}/>
+            <div className={styles['image']}>
+                <img alt={'게시글 이미지'} src={info.img}/>
+            </div>
         </div>
         <div className={styles['content']}>
-            <div className={styles['content-title']}>title</div>
-            <div className={styles['content-content']}>content</div>
-            <div className={styles['content-info']}>----년--일--일</div>
+            <div className={styles['content-title']}>{info.title}</div>
+            <div className={styles['content-content']}>{info.content}</div>
+            <div className={styles['content-info']}>{info.date}</div>
         </div>
         <div className={styles['footer']}>
-            <span className={styles['footer-writer']}>writer</span>
-            <span className={styles['footer-heart']}>10개</span>
+            <span className={styles['footer-writer']}>{info.user}</span>
+            <span className={styles['footer-heart']}>{info.heartNum}</span>
         </div>
     </div>
 }
