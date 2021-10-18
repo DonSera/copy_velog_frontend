207f60cc7e92e5c489d62c9690caf50b3bf1579a
DonSera <sera.2021@lifeoasis.com>
2021-10-18 11:17:27 +0900

    fix : 입력 후, 아무곳이나 누르면 modal이 닫히는 현상 수정 - 입력이 후 remove되는 과정에서 ref를 null로 만들어서 만들어진 버그

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -14,9 +14,6 @@ function Modal({header, body, footer, closeModal}) {
         window.addEventListener('click', handleRef);
         return () => {
             window.removeEventListener('click', handleRef);
-            if(modalRef.current){
-                modalRef.current = null;
-            }
         }
     })
 
