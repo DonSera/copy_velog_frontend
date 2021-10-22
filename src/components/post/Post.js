83290e88b04edc5dc04dac07b396efda315aa060
DonSera <sera.2021@lifeoasis.com>
2021-10-22 19:52:03 +0900

    fix : 게시물 목록 에서도 제목과 부제목 '#'이거 안보이게 설정

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -6,13 +6,27 @@ function Post({info}) {
         return date[0];
     }
 
+    function splitTitle() {
+        if (info.title.indexOf('#') === 0) {
+            return info.title.slice(2);
+        }
+        return info.title;
+    }
+
+    function splitSubTitle() {
+        if (info.subTitle.indexOf('#') === 0) {
+            return info.subTitle.slice(3);
+        }
+        return info.subTitle;
+    }
+
     return <div className={styles['post']}>
         <a className={styles['post-link']} href={`/post/${info.id}`}>
             <h1 className={styles['post-title']}>
-                {info.title}
+                {splitTitle()}
             </h1>
             <div className={styles['post-subTitle']}>
-                {info.subTitle}
+                {splitSubTitle()}
             </div>
             <div className={styles['post-date']}>
                 {splitDate()}
