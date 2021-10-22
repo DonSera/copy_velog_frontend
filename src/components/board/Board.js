6dee54afee4a9d7a991ca4408926734013bbb6a4
DonSera <sera.2021@lifeoasis.com>
2021-10-22 11:53:53 +0900

    fix : 제목과 부제목 render 방법 고치기 - 기존에 #이 처음으로 들어 간것과 아닌 것 구분하여 render

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -6,6 +6,20 @@ function Board({info}) {
         return date[0];
     }
 
+    function splitTitle() {
+        if (info.title.indexOf('#') === 0) {
+            return info.title.slice(2);
+        }
+        return info.title;
+    }
+
+    function splitSubTitle() {
+        if (info.subTitle.indexOf('#') === 0) {
+            return info.subTitle.slice(3);
+        }
+        return info.subTitle;
+    }
+
     return <div className={styles['board']}>
         <a className={styles['post-link']}
            href={`/post/${info.id}`}>
@@ -18,8 +32,8 @@ function Board({info}) {
         <div className={styles['content']}>
             <a className={`${styles['post-link']} ${styles['content-text']}`}
                href={`/post/${info.id}`}>
-                <h4 className={styles['content-title']}>{info.title}</h4>
-                <p className={styles['content-content']}>{info.subTitle}</p>
+                <h4 className={styles['content-title']}>{splitTitle()}</h4>
+                <p className={styles['content-content']}>{splitSubTitle()}</p>
             </a>
             <div className={styles['content-info']}>{splitDate()} {info.commentNum}개 댓글</div>
         </div>
