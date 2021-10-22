bfc79734b4ae0cc02838cdd641b84f54543e7cb3
DonSera <sera.2021@lifeoasis.com>
2021-10-22 15:54:56 +0900

    refactor : get 함수 분리하기

--- src/lib/server/get.js
+++ src/lib/server/get.js
@@ -2,17 +2,15 @@ import axios from "axios";
 
 const server = 'http://localhost:3001';
 
-export async function getMyPage(userName) {
-    const data = await axios.get(`${server}/myPage?name=${userName}`);
-    return data.data;
+async function get(url) {
+    const data = await axios.get(`${server}/${url}`);
+    return data.data
 }
 
-export async function getWriterName(postId) {
-    const data = await axios.get(`${server}/getWriterName?id=${postId}`);
-    return data.data;
+export async function getMyPage(userName) {
+    return await get(`myPage?name=${userName}`)
 }
 
 export async function getTagPost(tag) {
-    const data = await axios.get(`${server}/getTagPost?tag=${tag}`);
-    return data.data;
+    return get(`getTagPost?tag=${tag}`);
 }
